b526f4c4707eb4638766c920cc38d82096d10fe2
sssnewplus <habibudanlamidahir@gmail.com>
2024-11-25 17:33:49 +0100

    added migration dir

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # alchemy instance
 db = SQLAlchemy()
 
 # login mana. instance
 login_manager = LoginManager()
+
+
+# migrate instance. instance for db migrations
+migrate = Migrate()
 
 # func. to create app
 def create_app():
@@ -18,6 +23,29 @@ def create_app():
 
     # init. database
     db.init_app(app)
+
+    # init. migrate
+    migrate.init_app(app, db)
+
+    # init. login manager
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+    login_manager.login_message_category = 'info'
+
+    # user loader for the login manager
+    @login_manager.user_loader  
+    def user_loader(user_id):
+        from app.models import User
+        return User.query.get(user_id)
+
+    # registering routes/endpoints of the app
+    from app.admin import admin
+    from app.auth import auth
+    from app.views import view
+    app.register_blueprint(admin)
+    app.register_blueprint(auth)
+    app.register_blueprint(view)
+    return app
 
     # init. login manager
     login_manager.init_app(app)
